01fd9431f994f4abd085b54711536ce4a3ee9dc1
nilu26 <nkhot06@gmail.com>
2025-07-16 12:37:21 +0530

    exploring http methods- get,post,delete,etc.

--- tinder-be/src/app.js
+++ tinder-be/src/app.js
@@ -2,18 +2,29 @@ const express = require("express");
 
 const app = express();
 
+app.get("/user", (req,res) => {
+    res.send({firstname: "Nilesh", lastname: "khot"})
+})
 
-app.use("/nilesh", (req, res) => {
-    res.send("Hello from NK....!!");
-});
+app.post("/user", (req,res) => {
+    res.send("Data Saved Successfully");
+})
 
-app.use("/praju", (req, res) => {
-    res.send("Hello from PRAJU....!!");
-});
+app.delete("/user", (req, res) => {
+    res.send("User deleted Successfully");
+})
 
-app.use("/", (req, res) => {
-    res.send("Hello from Dashboard....!!");
-});
+// app.use("/nilesh", (req, res) => {
+//     res.send("Hello from NK....!!");
+// });
+
+// app.use("/praju", (req, res) => {
+//     res.send("Hello from PRAJU....!!");
+// });
+
+// app.use("/", (req, res) => {
+//     res.send("Hello from Dashboard....!!");
+// });
 
 app.listen(3001, () => {
     console.log("Server is successfully listening on port 3001... ")
